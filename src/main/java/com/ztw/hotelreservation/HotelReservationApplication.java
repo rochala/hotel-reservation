a926fdec2b60f59a74d1bfa7c58abf32cb126217
Jedrzej Rochala <rochala@tutanota.com>
2021-05-15 11:10:39 +0200

    add folder structure with empty files

--- src/main/java/com/ztw/hotelreservation/HotelReservationApplication.java
+++ src/main/java/com/ztw/hotelreservation/HotelReservationApplication.java
@@ -2,8 +2,9 @@ package com.ztw.hotelreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class HotelReservationApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ztw/hotelreservation/controller/MainController.java
+++ src/main/java/com/ztw/hotelreservation/controller/MainController.java
@@ -1,4 +1,9 @@
 package com.ztw.hotelreservation.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MainController {
+
+
 }
